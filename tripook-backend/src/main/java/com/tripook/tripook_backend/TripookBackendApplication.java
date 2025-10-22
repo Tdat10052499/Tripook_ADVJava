c65111b33dbe598f0dbf3c7e0993d44341fb14fc
Hồ Du Tuấn Đạt <tdat.100524@gmail.com>
2025-10-22 09:58:02 +0700

    Add files via upload

---
+++ tripook-backend/src/main/java/com/tripook/tripook_backend/TripookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tripook.tripook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripookBackendApplication.class, args);
+	}
+
+}
---
+++ tripook-backend/src/test/java/com/tripook/tripook_backend/TripookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripook.tripook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
